a354a17d8d7b3ec311fc4d3aa0b3bff9ff858021
suriya-prakash-murugan <suriya137prakash@gmail.com>
2025-07-27 20:43:34 -0700

    camera subscriber node added

---
+++ camera_tools/camera_tools/camera_subscriber.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class CameraSubscriber(Node):
+    def __init__(self):
+        super().__init__('camera_subscriber')
+        self.bridge = CvBridge()
+        self.subscription = self.create_subscription(
+            Image,
+            '/overhead_camera/image_raw',
+            self.listener_callback,
+            10)
+
+    def listener_callback(self, msg):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            cv2.imshow("Overhead Camera View", cv_image)
+            cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f'Image conversion failed: {e}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
